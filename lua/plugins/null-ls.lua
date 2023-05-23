a9a165d0f5d69c1749955bf21c48083be3b06b81
Dat Nguyen <datnguyen1215@gmail.com>
2023-05-23 09:47:34 -0700

    configure eslint.

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -6,10 +6,10 @@ return {
       root_dir = require("null-ls.utils").root_pattern(".null-ls-root", ".neoconf.json", "Makefile", ".git"),
       sources = {
         nls.builtins.formatting.prettierd,
-        --nls.builtins.formatting.fish_indent,
+        nls.builtins.formatting.fish_indent,
         nls.builtins.diagnostics.fish,
         nls.builtins.formatting.stylua,
-        --nls.builtins.formatting.shfmt,
+        nls.builtins.formatting.shfmt,
         nls.builtins.diagnostics.flake8,
       },
     }
