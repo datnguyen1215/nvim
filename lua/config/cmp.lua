eccfacaf0f99132381d74dd9f951b2219ba5ecee
Dat Nguyen <datnguyen1215@gmail.com>
2023-06-01 02:43:35 -0700

    Remove <CR> key and map <C-Space> to confirm selection.

--- lua/config/cmp.lua
+++ lua/config/cmp.lua
@@ -5,7 +5,10 @@ cmp.setup({
   mapping = cmp.mapping.preset.insert({
     ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
     ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
-    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+    ["<C-Space>"] = cmp.mapping.confirm({ select = true }),
+    ["<CR>"] = cmp.mapping(function(fallback)
+      fallback()
+    end),
     ["<Tab>"] = cmp.mapping(function(fallback)
       fallback()
     end),
