baabab883fee3481cfdcc2711814939383194c86
Dat Nguyen <dnguyen@screenbeam.com>
2023-06-03 01:19:45 -0700

    Update neo-tree settings.

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -3,6 +3,28 @@ return {
   opts = {
     filesystem = {
       bind_to_cwd = true,
+      window = {
+        mappings = {
+          ["/"] = "",
+        },
+      },
+    },
+    default_component_configs = {
+      git_status = {
+        symbols = {
+          -- Change type
+          added = "A", -- or "✚", but this is redundant info if you use git_status_colors on the name
+          modified = "M", -- or "", but this is redundant info if you use git_status_colors on the name
+          deleted = "D", -- this can only be used in the git_status source
+          renamed = "", -- this can only be used in the git_status source
+          -- Status type
+          untracked = "",
+          ignored = "",
+          unstaged = "",
+          staged = "",
+          conflict = "C",
+        },
+      },
     },
   },
   keys = {
