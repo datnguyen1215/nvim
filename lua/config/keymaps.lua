3773c412313461deb9a3eed02e11099479d82369
Dat Nguyen <datnguyen1215@gmail.com>
2023-06-18 20:13:49 -0700

    Remap <leader>q and <leader>w for :q and :w respectively.

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,3 +2,5 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
+vim.api.nvim_set_keymap('n', '<leader>q', ':q<CR>', { noremap = true, silent = true })
+vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { noremap = true, silent = true })
