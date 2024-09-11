438e3271600c156db367bb1c4dfb4c86b69e9e3f
Dat Nguyen <datnguyen1215@gmail.com>
2024-09-11 09:17:35 -0700

    update configuration for keymaps.

--- init.lua
+++ init.lua
@@ -1,10 +1,10 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 vim.g.autoformat = false
-require("config.mapleader")
-require("config.keymaps")
 require("config.lazy")
 require("config.copilot")
 require("config.cmp")
 require("config.telescope")
 require("config.colorscheme")
 require("config.tsserver")
+require("config.mapleader")
+require("config.keymaps")
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,17 +2,22 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Quit current pane
-vim.api.nvim_set_keymap('n', '<leader>q', ':q!<CR>', { silent = true })
-
 -- Save current pane
-vim.api.nvim_set_keymap('n', '<leader>w', ':w<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>ws", ":w<CR>", { silent = true })
 
--- Close all other buffers except the current one
-vim.api.nvim_set_keymap('n', '<leader>bo', ':%bd|e#|bd#<CR> :Neotree<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>wo", ":lua CloseAllExceptCurrentAndOpenNeotree()<CR>", { silent = true })
+
+function CloseAllExceptCurrentAndOpenNeotree()
+  vim.cmd("%bd")
+  vim.cmd("e#")
+  vim.cmd("bd#")
+  local cur_buf = vim.api.nvim_get_current_buf()
+  vim.cmd("Neotree")
+  vim.api.nvim_set_current_buf(cur_buf)
+end
 
 -- Split horizontally
-vim.api.nvim_set_keymap('n', '<leader>s', ':vs<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>s", ":vs<CR>", { silent = true })
 
 -- Split vertically
-vim.api.nvim_set_keymap('n', '<leader>S', ':split<CR>', { silent = true })
+vim.api.nvim_set_keymap("n", "<leader>S", ":split<CR>", { silent = true })
