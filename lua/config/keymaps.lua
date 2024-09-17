3767d2cdc058e43ad43477a973df803f7045fd2e
Dat Nguyen <datnguyen1215@gmail.com>
2024-09-16 20:30:50 -0700

    remove <leader>ww because we can save with <C-s>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -2,9 +2,6 @@
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
 
--- Save current pane
-vim.api.nvim_set_keymap("n", "<leader>ww", ":w<CR>", { silent = true })
-
 -- Split horizontally
 vim.api.nvim_set_keymap("n", "<leader>s", ":vs<CR>", { silent = true })
 
